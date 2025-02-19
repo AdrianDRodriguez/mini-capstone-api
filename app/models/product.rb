35052c2e1f1873c3bbb4f59194ccb573a08e47f0
Adrian Rodriguez <ar9897.r@gmail.com>
2025-02-18 21:38:22 -0600

    has_many, belongs_to categories

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,15 +1,4 @@
 class Product < ApplicationRecord
-  validates :name, presence: true
-  validates :name, length: { in: 1..7 }
-  validates :price, presence: true
-  validates :price, numericality: { greater_than: 5, less_than_or_equal_to: 50 }
-  validates :description, presence: true
-  validates :description, length: { in: 1..35 }
-
-  def is_discounted?
-    price <= 15
-    if price <10
-      put "This is a super discount!!"
-    end
-  end
+  has_many :category_products
+  has_many :categories, through: :category_products
 end
