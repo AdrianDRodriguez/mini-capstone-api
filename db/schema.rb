4042d7d148f45cebbfe317a1bfb1d3d659a1d211
Adrian Rodriguez <ar9897.r@gmail.com>
2025-02-25 18:41:11 -0600

    Orders model, jbuilder and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_19_013416) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_26_001718) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,17 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_19_013416) do
   create_table "category_products", force: :cascade do |t|
     t.integer "product_id"
     t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.integer "quantity"
+    t.decimal "subtotal", precision: 9, scale: 2
+    t.decimal "tax", precision: 9, scale: 2
+    t.decimal "total", precision: 9, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
