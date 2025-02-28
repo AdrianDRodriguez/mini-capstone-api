14a13be98707e0b3748db478daa10b0cecc0cf76
Adrian Rodriguez <ar9897.r@gmail.com>
2025-02-27 19:19:34 -0600

    In progress of orders action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,19 @@
 class OrdersController < ApplicationController
+  def index
+    @orders = Order.all
+    render :index
+  end
+
+  def create
+    @order = Order.new(
+      user_id: params[:user_id],
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      subtotal: params[:subtotal],
+      tax: params[:tax],
+      total: params[:total],
+    )
+    @order.save
+    render :show
+  end
 end
