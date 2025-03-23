6a8d09a305ebf7f5c1ebb90a16e697f72fab4a3e
Adrian Rodriguez <ar9897.r@gmail.com>
2025-03-23 16:07:43 -0500

    Categories (not finished)

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,6 @@
+class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+    render template: "categories/index"
+  end
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ApplicationRecord
-  has_many :category_products
+  has_many :category_product
   has_many :categories, through: :category_products
 end
